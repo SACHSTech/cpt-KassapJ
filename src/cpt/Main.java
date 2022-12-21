d1b55a1f77ad54a363b872487e7cc8c8fce87589
John Matthew Kassapian <matthewkassapian4@gmail.com>
2022-12-21 17:29:08 -0500

    V1.5 - Starting the authorization process

--- src/cpt/Main.java
+++ src/cpt/Main.java
@@ -2,6 +2,9 @@ package cpt;
 
 public class Main{
     public static void Main(String[] args) throws Exception{
-        
+        String client_id = "8c2fedf8d39e4e4b831dde153af912ff"; // The app's client id
+        String client_secret = "a7646a64433740338b2759e87edfad7d"; // The app's secret
+        String redirect_uri = "http://127.0.0.1:5500/"; // Redirect URI
+        String scopes = "user-read-private user-read-email"; // Scope that allows app to request access to the user's data
     }
 }
