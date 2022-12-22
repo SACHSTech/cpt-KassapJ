780674bf61e0e36a6222058e4dad9f3cd5b67bc2
John Matthew Kassapian <johnmatthew.kassapian23@ycdsbk12.ca>
2022-12-22 09:46:44 -0500

    V2.5 - FX working for new file

--- src/cpt/Main.java
+++ src/cpt/Main.java
@@ -22,7 +22,7 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application{
-    public static void Main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception{
         String client_id = "8c2fedf8d39e4e4b831dde153af912ff"; // The app's client id
         String client_secret = "a7646a64433740338b2759e87edfad7d"; // The app's secret
         String redirect_uri = "http://127.0.0.1:5500/"; // Redirect URI
