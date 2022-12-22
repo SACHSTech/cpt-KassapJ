67f20355a10a7cee38250a530ae1176963884153
John Matthew Kassapian <johnmatthew.kassapian23@ycdsbk12.ca>
2022-12-22 09:28:55 -0500

    V2 - Making JavaFX work in the main file so I can start

--- src/cpt/Main.java
+++ src/cpt/Main.java
@@ -1,10 +1,52 @@
 package cpt;
 
-public class Main{
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import static javafx.geometry.HPos.RIGHT;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+
+public class Main extends Application{
     public static void Main(String[] args) throws Exception{
         String client_id = "8c2fedf8d39e4e4b831dde153af912ff"; // The app's client id
         String client_secret = "a7646a64433740338b2759e87edfad7d"; // The app's secret
         String redirect_uri = "http://127.0.0.1:5500/"; // Redirect URI
         String scopes = "user-read-private user-read-email"; // Scope that allows app to request access to the user's data
+    
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage){
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
     }
 }
