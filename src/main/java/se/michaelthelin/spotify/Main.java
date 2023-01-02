155dcaddc5b0ad67eaf021b1f518818c8cf6f2ba
John Matthew Kassapian <matthewkassapian4@gmail.com>
2023-01-02 15:10:46 -0500

    V4 - Interacting with spotify wrapper

--- src/main/java/se/michaelthelin/spotify/Main.java
+++ src/main/java/se/michaelthelin/spotify/Main.java
@@ -18,7 +18,7 @@ import com.wrapper.spotify.requests.authorization.client_credentials.ClientCrede
 
 // Main application
 public class Main extends Application{
-    public static void Main(String[] args) throws Exception{
+    public static void Builder(String[] args) throws Exception{
         String client_id = "8c2fedf8d39e4e4b831dde153af912ff"; // The app's client id
         String client_secret = "a7646a64433740338b2759e87edfad7d"; // The app's secret
         String redirect_uri = "http://127.0.0.1:5500/"; // Redirect URI
