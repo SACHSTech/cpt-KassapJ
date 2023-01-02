4bbf02d4e7b5461a4520007de847d68b1060b666
John Matthew Kassapian <matthewkassapian4@gmail.com>
2023-01-02 18:20:34 -0500

    V8 - I'm done for tonight, created concepts though

---
+++ src/cpt/Main.java
@@ -0,0 +1,10 @@
+package cpt;
+/*
+ * This is the program that is going to create an object to get the data, which willc reate various other objects that can be serperated by attributes
+ * This program however will take that data and be able to filter it out by different properties
+ * This program will take that data and put it onto a data visualizer of some sort, probably one line chart and one bar graph
+ * This program will also sort through it depending on what the user wants (least to greatest) etc.
+ */
+public class Main {
+    
+}
