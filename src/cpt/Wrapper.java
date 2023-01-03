72f0ea15224b809609079b6ba2929a7fb087884d
John Matthew Kassapian <matthewkassapian4@gmail.com>
2023-01-02 21:42:42 -0500

    V8 no leaks

--- src/cpt/Wrapper.java
+++ src/cpt/Wrapper.java
@@ -19,9 +19,9 @@ import com.wrapper.spotify.requests.authorization.client_credentials.ClientCrede
 // Main application
 public class Wrapper extends Application{
     public static void main(String[] args) throws Exception{
-        String client_id = "8c2fedf8d39e4e4b831dde153af912ff"; // The app's client id
-        String client_secret = "a7646a64433740338b2759e87edfad7d"; // The app's secret
-        String redirect_uri = "http://127.0.0.1:5500/"; // Redirect URI
+        String client_id = "client"; // The app's client id
+        String client_secret = "secret"; // The app's secret
+        String redirect_uri = "uri"; // Redirect URI
         String scopes = "user-read-private user-read-email"; // Scope that allows app to request access to the user's data
 
         // Building the communication with the api that will retrieve thevlistening data of the user for us
