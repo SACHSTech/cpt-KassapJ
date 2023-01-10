7ad31d0697a271790c3fe32804b9498a5bdf4ad1
John Matthew Kassapian <johnmatthew.kassapian23@ycdsbk12.ca>
2023-01-10 08:49:36 -0500

    V10 - Preparing files to create UI

--- src/cpt/Main.java
+++ src/cpt/Main.java
@@ -1,10 +1,37 @@
 package cpt;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.stage.Stage;
+
 /*
  * This is the program that is going to create an object to get the data, which willc reate various other objects that can be serperated by attributes
  * This program however will take that data and be able to filter it out by different properties
  * This program will take that data and put it onto a data visualizer of some sort, probably one line chart and one bar graph
  * This program will also sort through it depending on what the user wants (least to greatest) etc.
  */
-public class Main {
+public class Main extends Application{
     
+    @Override public void start(Stage primaryStage) throws Exception {
+        
+    }
+
+    public Parent createContent() {
+        
+    }
+
+    /**
+     * Java main for when running without JavaFX launcher
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
 }
