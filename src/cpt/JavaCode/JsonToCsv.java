ca089b24bdb5dbcd81cb7ca850705455b747dbae
John Matthew Kassapian <johnmatthew.kassapian23@ycdsbk12.ca>
2023-01-16 11:02:04 -0500

    CONVERSION WORKS

--- src/cpt/JavaCode/JsonToCsv.java
+++ src/cpt/JavaCode/JsonToCsv.java
@@ -1,9 +1,6 @@
 package cpt.JavaCode;
 
 import java.io.*;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -14,15 +11,30 @@ public class JsonToCsv {
 
     public static void main(String[] args) {
         JSONParser parser = new JSONParser();
-        String directory =  "src/cpt/SpotifyJsonFilesHERE/DROPFILESHERE.txt";
-        String[] files = directory.list()
-        //choosing the file to read
-        try (FileReader reader = new FileReader("src/cpt/SpotifyJsonFilesHERE/StreamingHistory0.json"))
-        {
+        File directory =  new File("src/cpt/SpotifyJsonFilesHERE");
+        String[] files = directory.list();
+        int intJsonAmount = files.length - 1;
+        String csv = "";
+        double jsonArraySize = 0;
+        double count = 0;
+        
+        //Go through all the streaming history files
+        for(int i = 0; i < intJsonAmount; i++){
+            try (FileReader reader = new FileReader("src/cpt/SpotifyJsonFilesHERE/StreamingHistory" + i + ".json")){
             // identifying the json array we're reading through
             Object obj = parser.parse(reader);
             JSONArray jsonArray = (JSONArray) obj;
-            String csv = "";
+            jsonArraySize += jsonArray.size();
+            }catch (IOException | ParseException e) {
+                e.printStackTrace();
+            }
+        }
+
+        for(int i = 0; i < intJsonAmount; i++){
+            try (FileReader reader = new FileReader("src/cpt/SpotifyJsonFilesHERE/StreamingHistory" + i + ".json")){
+            // identifying the json array we're reading through
+            Object obj = parser.parse(reader);
+            JSONArray jsonArray = (JSONArray) obj;
             // Creating a string text to format the csv
             for (Object jsonObject : jsonArray) {
                 JSONObject json = (JSONObject) jsonObject;
@@ -30,15 +42,20 @@ public class JsonToCsv {
                     csv += json.get(key) + ",";
                 }
                 csv += "\n";
+                count++;
+                System.out.println("Processing " + (String.format("%.0f%%",(count / jsonArraySize)*100)));
             }
-
+            System.out.println("File " + (i+1) + " complete");
+            }catch (IOException | ParseException e) {
+                e.printStackTrace();
+            }
+        }
             // writing the csv file
             try (FileWriter writer = new FileWriter("src/cpt/ConvertedFiles/data.csv")) {
                 writer.write(csv);
                 writer.flush();
-            }
-        } catch (IOException | ParseException e) {
+            }catch (IOException e) {
             e.printStackTrace();
-        }
+            }
     }
 }
